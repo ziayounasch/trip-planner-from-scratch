31415b3c5179a360385f833e8633a8bba51bccde
ziayounasch <ziayounasch04@gmail.com>
2024-07-23 12:36:12 +0500

    pysqlite3 added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from main import TripCrew  # Import the ResearchCrew class from main.py
 import datetime
